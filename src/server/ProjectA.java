e229776f8f39002a0bc48f568f18599c72cc4ac1
rlfalsgh95 <rlfalsgh95@naver.com>
2020-06-07 21:22:03 +0900

    initial commit

---
+++ src/server/ProjectA.java
@@ -0,0 +1,28 @@
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProjectA
+ */
+@WebServlet("/ProjectA")
+public class ProjectA extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ProjectA() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+	}
+
+}
